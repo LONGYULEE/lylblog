2a6b2b628b19369bd69e790ff4ed9aa80a146f62
Longyulee <1097910340@qq.com>
2020-05-23 23:54:49 +0800

    article

--- myblog-index/src/main/java/com/liyulong/blog/index/article/controller/ArticleController.java
+++ myblog-index/src/main/java/com/liyulong/blog/index/article/controller/ArticleController.java
@@ -1,7 +1,43 @@
 package com.liyulong.blog.index.article.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.liyulong.blog.index.article.service.ArticleService;
+import com.liyulong.blog.main.common.constant.RedisCacheNames;
+import com.liyulong.blog.main.common.result.Result;
+import com.liyulong.blog.main.common.result.ResultUtil;
+import com.liyulong.blog.main.common.util.PageUtils;
+import com.liyulong.blog.main.pojo.article.vo.ArticleVO;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import javax.annotation.Resource;
+import java.util.Map;
+
+@RestController("articlePortalController")
+@CacheConfig(cacheNames = {RedisCacheNames.ARTICLE})
 public class ArticleController {
+
+    @Resource
+    private ArticleService articleService;
+
+    @GetMapping("/article/{articleId}")
+//    @LogView(type = "article")
+    public Result getArticle(@PathVariable Integer articleId){
+        ArticleVO article = articleService.getArticleVo(articleId);
+        return ResultUtil.success(article);
+    }
+
+    @PutMapping("/article/like/{id}")
+//    @LogLike(type = "article")
+    public Result likeArticle(@PathVariable Integer id) {
+        return ResultUtil.success();
+    }
+
+    @GetMapping("/articles")
+    @Cacheable
+    public Result list(@RequestParam Map<String, Object> params){
+        PageUtils page = articleService.queryPageCondition(params);
+        return ResultUtil.success(page);
+    }
+
 }
--- myblog-index/src/main/java/com/liyulong/blog/index/article/service/impl/ArticleServiceImpl.java
+++ myblog-index/src/main/java/com/liyulong/blog/index/article/service/impl/ArticleServiceImpl.java
@@ -1,29 +1,50 @@
 package com.liyulong.blog.index.article.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.liyulong.blog.index.article.service.ArticleService;
+import com.liyulong.blog.main.common.enums.ModuleEnum;
 import com.liyulong.blog.main.common.util.PageUtils;
+import com.liyulong.blog.main.common.util.Query;
 import com.liyulong.blog.main.mapper.article.ArticleMapper;
 import com.liyulong.blog.main.pojo.article.Article;
 import com.liyulong.blog.main.pojo.article.vo.ArticleVO;
+import com.liyulong.blog.manage.operation.service.TagService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.List;
 import java.util.Map;
 
 @Service("ArticleIndexService")
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> implements ArticleService {
+
+    @Resource
+    private TagService tagService;
+
     @Override
     public PageUtils queryPageCondition(Map<String, Object> params) {
-        return null;
+        Page<ArticleVO> page = new Query<ArticleVO>(params).getPage();
+        List<ArticleVO> articleList = baseMapper.queryPageCondition(page, params);
+        // 封装ArticleVo
+        page.setRecords(articleList);
+        return new PageUtils(page);
     }
 
     @Override
     public ArticleVO getArticleVo(Integer articleId) {
-        return null;
+        Article article = baseMapper.selectById(articleId);
+        ArticleVO articleVo = new ArticleVO();
+        BeanUtils.copyProperties(article,articleVo);
+        articleVo.setTagList(tagService.listByLinkId(articleId, ModuleEnum.ARTICLE.getValue()));
+        return articleVo;
     }
 
     @Override
     public ArticleVO getSimpleArticleVo(Integer articleId) {
-        return null;
+        ArticleVO articleVo = baseMapper.getSimpleArticleVo(articleId);
+        articleVo.setTagList(tagService.listByLinkId(articleId,ModuleEnum.ARTICLE.getValue()));
+        return articleVo;
     }
 }
