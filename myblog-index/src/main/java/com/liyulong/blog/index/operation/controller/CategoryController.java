e41074b39ac2ba143d9e9c3e5105f0958ea2341e
Longyulee <1097910340@qq.com>
2020-05-23 23:19:56 +0800

    category

--- myblog-index/src/main/java/com/liyulong/blog/index/operation/controller/CategoryController.java
+++ myblog-index/src/main/java/com/liyulong/blog/index/operation/controller/CategoryController.java
@@ -1,4 +1,34 @@
 package com.liyulong.blog.index.operation.controller;
 
+import com.liyulong.blog.index.operation.service.CategoryService;
+import com.liyulong.blog.main.common.constant.RedisCacheNames;
+import com.liyulong.blog.main.common.result.Result;
+import com.liyulong.blog.main.common.result.ResultUtil;
+import com.liyulong.blog.main.pojo.operation.Category;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@RestController("categoryPortalController")
+@CacheConfig(cacheNames = RedisCacheNames.CATEGORY)
+@RequestMapping("/operation")
 public class CategoryController {
+
+    @Resource
+    private CategoryService categoryService;
+
+    @GetMapping("/categories")
+    @Cacheable
+    public Result listCategory(@RequestParam Map<String,Object> params) {
+        List<Category> categoryList = categoryService.listCategory(params);
+        return ResultUtil.success(categoryList);
+    }
+
 }
--- myblog-index/src/main/java/com/liyulong/blog/index/operation/service/impl/CategoryServiceImpl.java
+++ myblog-index/src/main/java/com/liyulong/blog/index/operation/service/impl/CategoryServiceImpl.java
@@ -1,9 +1,24 @@
 package com.liyulong.blog.index.operation.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.liyulong.blog.index.operation.service.CategoryService;
 import com.liyulong.blog.main.mapper.operation.CategoryMapper;
 import com.liyulong.blog.main.pojo.operation.Category;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
+@Service("CategoryIndexService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+    @Override
+    public List<Category> listCategory(Map<String, Object> params) {
+        String type = (String) params.get("type");
+        String rank = (String) params.get("rank");
+        return baseMapper.selectList(new QueryWrapper<Category>().lambda()
+                .eq(StringUtils.isNotEmpty(type),Category::getType,type)
+                .eq(StringUtils.isNotEmpty(rank),Category::getRank,rank));
+    }
 }
